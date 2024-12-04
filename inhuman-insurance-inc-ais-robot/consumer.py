ffb129e70ed01cf76afca0e2172131e3b7a053ea
Thiago Martins <thiago.fmartins@outlook.com>
2024-12-04 14:02:29 -0300

    Refact: Splitting the tasks file into consumer and producer files

---
+++ inhuman-insurance-inc-ais-robot/consumer.py
@@ -0,0 +1,38 @@
+
+from robocorp.tasks import task
+from robocorp import workitems
+import requests
+
+@task
+def consume_traffic_data():
+    """
+    Inhuman Insurance, Inc. Artificial Intelligence System automation.
+    Consumes traffic data work items.
+    """
+    for item in workitems.inputs:
+        traffic_data = item.payload["traffic_data"]
+        valid = validate_traffic_data(traffic_data)
+        if valid:
+            status, return_json = post_traffic_data_to_sales_system(traffic_data)
+            if status == 200:
+                item.done()
+            else: 
+                item.fail(
+                    exception_type="APPLICATION",
+                    code="TRAFFIC_DATA_POST_FAILED",
+                    message=return_json["message"],
+                )
+        else:
+            item.fail(
+                exception_type="BUSINESS",
+                code="INVALID_TRAFFIC_DATA",
+                message=item.payload,
+            )
+
+def validate_traffic_data(traffic_data) -> bool:
+    return len(traffic_data["country"]) == 3
+    
+def post_traffic_data_to_sales_system(traffic_data) -> tuple[int, dict]:
+    url = "https://robocorp.com/inhuman-insurance-inc/sales-system-api"
+    response = requests.post(url, json=traffic_data)
+    return response.status_code, response.json()
--- inhuman-insurance-inc-ais-robot/producer.py
+++ inhuman-insurance-inc-ais-robot/producer.py
@@ -1,9 +1,8 @@
-from robocorp import workitems
 from robocorp.tasks import task
 from RPA.HTTP import HTTP
 from RPA.JSON import JSON
 from RPA.Tables import Tables, Table
-import requests
+from robocorp import workitems
 
 http = HTTP()
 json = JSON()
@@ -36,40 +35,6 @@ def produce_traffic_data():
     filtered_data = get_latest_data_by_country(filtered_data)
     payloads = create_work_item_payloads(filtered_data)
     save_work_item_payloads(payloads)
-
-@task
-def consume_traffic_data():
-    """
-    Inhuman Insurance, Inc. Artificial Intelligence System automation.
-    Consumes traffic data work items.
-    """
-    for item in workitems.inputs:
-        traffic_data = item.payload["traffic_data"]
-        valid = validate_traffic_data(traffic_data)
-        if valid:
-            status, return_json = post_traffic_data_to_sales_system(traffic_data)
-            if status == 200:
-                item.done()
-            else: 
-                item.fail(
-                    exception_type="APPLICATION",
-                    code="TRAFFIC_DATA_POST_FAILED",
-                    message=return_json["message"],
-                )
-        else:
-            item.fail(
-                exception_type="BUSINESS",
-                code="INVALID_TRAFFIC_DATA",
-                message=item.payload,
-            )
-
-def validate_traffic_data(traffic_data) -> bool:
-    return len(traffic_data["country"]) == 3
-    
-def post_traffic_data_to_sales_system(traffic_data) -> tuple[int, dict]:
-    url = "https://robocorp.com/inhuman-insurance-inc/sales-system-api"
-    response = requests.post(url, json=traffic_data)
-    return response.status_code, response.json()
 
 def load_traffic_data_as_table() -> Table:
     json_data = json.load_json_from_file(TRAFFIC_JSON_FILE_PATH)
