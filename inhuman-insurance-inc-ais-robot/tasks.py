57cc4fd1d70210136a8b48fecaaa4bd7a2ec2deb
Thiago Martins <thiago.fmartins@outlook.com>
2024-12-04 11:38:10 -0300

    Persisting the data to work items in  produce_traffic_data task

--- inhuman-insurance-inc-ais-robot/tasks.py
+++ inhuman-insurance-inc-ais-robot/tasks.py
@@ -1,3 +1,4 @@
+from robocorp import workitems
 from robocorp.tasks import task
 from RPA.HTTP import HTTP
 from RPA.JSON import JSON
@@ -8,6 +9,12 @@ json = JSON()
 table = Tables()
 
 TRAFFIC_JSON_FILE_PATH = "output/traffic.json"
+
+# JSON data keys
+COUNTRY_KEY = "SpatialDim"
+YEAR_KEY = "TimeDim"
+RATE_KEY = "NumericValue"
+GENDER_KEY = "Dim1"
 
 
 @task
@@ -24,7 +31,10 @@ def produce_traffic_data():
     )
     traffic_data = load_traffic_data_as_table()
     table.write_table_to_csv(traffic_data, "output/test.csv")
-
+    filtered_data = filter_and_sort_traffic_data(traffic_data)
+    filtered_data = get_latest_data_by_country(filtered_data)
+    payloads = create_work_item_payloads(filtered_data)
+    save_work_item_payloads(payloads)
 
 @task
 def consume_traffic_data():
@@ -38,3 +48,36 @@ def consume_traffic_data():
 def load_traffic_data_as_table():
     json_data = json.load_json_from_file(TRAFFIC_JSON_FILE_PATH)
     return table.create_table(json_data["value"])
+
+
+def filter_and_sort_traffic_data(data):
+    table.filter_table_by_column(data, RATE_KEY, "<", 5.0)
+    table.filter_table_by_column(data, GENDER_KEY, "==", "BTSX")
+    table.sort_table_by_column(data, YEAR_KEY, False)
+    return data
+
+def get_latest_data_by_country(data):
+    data = table.group_table_by_column(data, COUNTRY_KEY)
+    latest_data_by_country = []
+    for group in data:
+        first_row = table.pop_table_row(group)
+        latest_data_by_country.append(first_row)
+    return latest_data_by_country
+
+def create_work_item_payloads(data):
+    payloads = []
+    for row in data:
+        payload = {
+            "country": row[COUNTRY_KEY],
+            "year": row[YEAR_KEY],
+            "rate": row[RATE_KEY],
+        }
+        payloads.append(payload)
+    return payloads
+
+def save_work_item_payloads(payloads):
+    for payload in payloads:
+        variables = {
+            "traffic_data": payload
+        }
+        workitems.outputs.create(variables)
