4989f4719c2835f200bd3319fdc9f161b9cac51e
Thiago Martins <thiago.fmartins@outlook.com>
2024-12-04 00:24:17 -0300

    Add extraction and transform steps into producer task

--- inhuman-insurance-inc-ais-robot/tasks.py
+++ inhuman-insurance-inc-ais-robot/tasks.py
@@ -1,6 +1,40 @@
 from robocorp.tasks import task
+from RPA.HTTP import HTTP
+from RPA.JSON import JSON
+from RPA.Tables import Tables
+
+http = HTTP()
+json = JSON()
+table = Tables()
+
+TRAFFIC_JSON_FILE_PATH = "output/traffic.json"
+
 
 @task
-def minimal_task():
-    message = "Hello"
-    message = message + " World!"
+def produce_traffic_data():
+    """
+    Inhuman Insurance, Inc. Artificial Intelligence System automation.
+    Produces traffic data work items.
+    """
+    print("produce")
+    http.download(
+        url="https://github.com/robocorp/inhuman-insurance-inc/raw/main/RS_198.json",
+        target_file=TRAFFIC_JSON_FILE_PATH,
+        overwrite=True,
+    )
+    traffic_data = load_traffic_data_as_table()
+    table.write_table_to_csv(traffic_data, "output/test.csv")
+
+
+@task
+def consume_traffic_data():
+    """
+    Inhuman Insurance, Inc. Artificial Intelligence System automation.
+    Consumes traffic data work items.
+    """
+    print("consume")
+
+
+def load_traffic_data_as_table():
+    json_data = json.load_json_from_file(TRAFFIC_JSON_FILE_PATH)
+    return table.create_table(json_data["value"])
